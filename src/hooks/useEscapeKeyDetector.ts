c5fab112e927a4381675ec5f4952a5a539df4f8a
Arsh <arsh@Arshs-MacBook-Air.local>
2024-12-18 18:44:51 +0530

    initial setup

---
+++ src/hooks/useEscapeKeyDetector.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+const useEscapeKeyDetector = <P extends HTMLElement>(handler: () => void) => {
+  const ref = useRef<P>(null);
+  useEffect(() => {
+    function handleEscapeKey(event: KeyboardEvent) {
+      if (event.keyCode === 27) {
+        handler();
+      }
+    }
+
+    document.addEventListener('keydown', handleEscapeKey);
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, []);
+
+  return ref;
+};
+
+export default useEscapeKeyDetector;
